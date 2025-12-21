9dd9de1a9142bf59ed64930b878e0d6d41c1fa40
Gavino1730 <gavino1730@gmail.com>
2025-12-20 22:26:02 -0800

    Update admin credentials to Boyaca1730!

--- CREATE_ADMIN.sql
+++ CREATE_ADMIN.sql
@@ -1,4 +1,4 @@
--- METHOD 1: Register through the website with username "admin" and password "12345678"
+-- METHOD 1: Register through the website with username "admin" and password "Boyaca1730!"
 -- Then run this SQL to make that user an admin:
 
 UPDATE users 
@@ -7,17 +7,3 @@ WHERE username = 'admin';
 
 -- METHOD 2: If you already have a user, make them admin:
 -- UPDATE users SET is_admin = true, balance = 10000 WHERE username = 'yourusername';
-
--- METHOD 3: Create admin directly (may have password hash issues)
--- If this doesn't work, use METHOD 1 instead
-INSERT INTO users (username, password, balance, is_admin)
-VALUES (
-  'admin',
-  '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-  10000,
-  true
-)
-ON CONFLICT (username) 
-DO UPDATE SET 
-  is_admin = true,
-  balance = 10000;
